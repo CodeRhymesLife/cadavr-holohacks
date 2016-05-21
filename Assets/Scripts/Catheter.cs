f67647dfa494e9c66c94900dfbe14296fe5d50f9
Ryan James <rcjames1004@gmail.com>
2016-05-21 02:24:48 -0700

    Add catheter

---
+++ Assets/Scripts/Catheter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Catheter : MonoBehaviour {
+
+	void MoveUp()
+    {
+
+    }
+
+    void MoveDown()
+    {
+
+    }
+
+    void RotateLeft()
+    {
+
+    }
+
+    void RotateRight()
+    {
+
+    }
+}
