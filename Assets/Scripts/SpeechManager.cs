fc0edd1c372998ee0a0b57d004633efd6d084366
Ryan James <rcjames1004@gmail.com>
2016-05-21 04:53:05 -0700

    Zoom in and out of the heart

--- Assets/Scripts/SpeechManager.cs
+++ Assets/Scripts/SpeechManager.cs
@@ -7,13 +7,28 @@ public class SpeechManager : MonoBehaviour
 {
     KeywordRecognizer keywordRecognizer = null;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    public GameObject heart;
     public Catheter catheter;
     public float speed;
     private Rigidbody rb;
 
+    private InitialHeartData initialHeartData;
+
+    private class InitialHeartData
+    {
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
     // Use this for initialization
     void Start()
     {
+        initialHeartData = new InitialHeartData
+        {
+            position = heart.transform.position,
+            scale = heart.transform.localScale,
+        };
+
         rb = catheter.GetComponent<Rigidbody>();
         keywords.Add("Move Up", () =>
         {
@@ -35,6 +50,18 @@ public class SpeechManager : MonoBehaviour
         keywords.Add("Rotate Right", () =>
         {
             catheter.RotateRight();
+        });
+
+        keywords.Add("Zoom In", () =>
+        {
+            heart.transform.localScale *= 30;
+            heart.transform.position = Camera.main.gameObject.transform.position + (heart.transform.position - catheter.transform.position);
+        });
+
+        keywords.Add("Zoom Out", () =>
+        {
+            heart.transform.localScale = initialHeartData.scale;
+            heart.transform.position = initialHeartData.position;
         });
 
         // Tell the KeywordRecognizer about our keywords.
